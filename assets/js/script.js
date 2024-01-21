aac2ed6fa6e50d75973685ad6c0201cf0ea210da
Ioana Iosub <stu.waltham@gmail.com>
2024-01-21 10:46:37 +0000

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-// I'm going to write the codes soon
+//Here will go the dictionary work
+
+function displayDisctionary(event) {
+  event.preventDefault();
+  const hidden = document.getElementById("book-search");
+  hidden.element.classList.add("hide-book-search");
+}
+
+document
+  .getElementById("dictionaty")
+  .addEventListener("click", displayDisctionary);
+// Here will the  dictionary work
