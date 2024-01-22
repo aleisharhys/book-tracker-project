27a0a587fa68ae97cecfe9cb06ce048a812f2dfd
Ioana Iosub <stu.waltham@gmail.com>
2024-01-22 21:13:08 +0000

    Word definition implimented

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,12 @@ function displayDictionary(event) {
 
   // add a class display so we can display the dictionary
   const display = document.querySelector(".dictionary-search");
+  display.classList.remove("hide-search"); // Use remove instead of add
   display.classList.add("display-dictionary");
 
   const button = document.querySelector(".dictionary-search-btn");
+  const wordSound = document.querySelector(".sound");
+  const searchResult = document.querySelector(".search-result");
 
   button.addEventListener("click", () => {
     const inputValue = document.getElementById("dictionary-search").value;
@@ -21,7 +24,17 @@ function displayDictionary(event) {
 
     fetch(`${dictionaryURL}${inputValue}`)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        searchResult.innerHTML = `<div class="word">
+                    <h2 class="sample">${inputValue}</h2>
+                    <button class="sound"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
+                <div class="word-details">
+                <p class="meaning">${data[0].meanings[0].definitions[0].definition}</p>
+                <p>
+                </div>`;
+      });
   });
 }
 
