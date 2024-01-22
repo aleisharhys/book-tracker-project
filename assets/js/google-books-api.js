0850f2c4c2ac8f31dacb43262442c596f530ae23
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-01-22 16:21:17 +0100

    displaying 5 search results

--- assets/js/google-books-api.js
+++ assets/js/google-books-api.js
@@ -1,4 +1,5 @@
 const searchBtn = $('#search-btn');
+const bookSearchForm = $('#book-search-form');
 
 searchBtn.on('click', function (e) {
     e.preventDefault();
@@ -15,6 +16,24 @@ function searchBook(url) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
+            const results = [data.items[0], data.items[1], data.items[2], data.items[3], data.items[5]];
+            console.log(results)
+            bookSearchForm.addClass('hidden');
+            const tableEl = $('#results');
+
+            results.forEach(result => {
+                const rowEl = $('<tr>');
+                const coverCell = $('<td>');
+                const coverImg = $('<img>').attr('src', result.volumeInfo.imageLinks.smallThumbnail);
+                const authorAndTitleCell = $('<td>').text(`${result.volumeInfo.authors[0]} - ${result.volumeInfo.title}`);
+                const descriptionCell = $('<td>').text(result.volumeInfo.description);
+                const addButtonCell = $('<td>');
+                const addButton = $('<button>').text('Add to my collection');
+
+                coverCell.append(coverImg);
+                addButtonCell.append(addButton);
+                rowEl.append(coverCell, authorAndTitleCell, descriptionCell, addButtonCell);
+                tableEl.append(rowEl);
+            })
         })
 }
