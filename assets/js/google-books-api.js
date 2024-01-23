c24040edca93aaeb7d084e0f157e3954539f4480
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-01-23 01:17:39 +0100

    updated code structure, more comments, some fixes, etc.

--- assets/js/google-books-api.js
+++ assets/js/google-books-api.js
@@ -11,13 +11,49 @@ const dictionaryDiv = $('#dictionary-div');
 
 let collection = [];
 
+//This function controls the menu sections in the navbar. It decides what to show and what to hide once a section is clicked.
+function showNavbarContent() {
+    dictionaryNav.on('click', function () {
+        dictionaryDiv.removeClass('hidden');
+        searchDiv.addClass('hidden');
+        resultsDiv.addClass('hidden');
+        myBooksDiv.addClass('hidden');
+    })
+
+    searchNav.on('click', function () {
+        $('#book-search').val('');
+        searchDiv.removeClass('hidden');
+        dictionaryDiv.addClass('hidden');
+        resultsDiv.addClass('hidden');
+        myBooksDiv.addClass('hidden');
+    })
+
+    myBooksNav.on('click', function () {
+        loadMyBooks();
+        saveReadState();
+        loadReadState();
+        myBooksDiv.removeClass('hidden');
+        dictionaryDiv.addClass('hidden');
+        searchDiv.addClass('hidden');
+        resultsDiv.addClass('hidden');
+    })
+}
+
+showNavbarContent();
+
 // Event listener on the search button.
 searchBtn.on('click', function (e) {
     e.preventDefault();
 
+    // Before displaying the current search results, previous results are getting cleared.
     resultsTable.text('');
+
+    // Hide the search input before rendering the results.
     searchDiv.addClass('hidden');
+
+    // Make results visible by removing the hidden class.
     resultsDiv.removeClass('hidden');
+
     const searchTerm = $('#book-search').val().trim();
     const queryURL = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`;
 
@@ -36,18 +72,33 @@ function searchBook(url) {
         .then(function (data) {
             const results = [data.items[0], data.items[1], data.items[2], data.items[3], data.items[5]];
             const tableEl = $('#results');
-            console.log(results)
+            console.log(results[1])
+
+            // Rendering the search results to the screen.
             results.forEach(result => {
                 const rowEl = $('<tr>');
                 const coverCell = $('<td>');
-                const coverImg = $('<img>').attr('src', result.volumeInfo.imageLinks.smallThumbnail);
+
+                // If no cover picture available, it will be replaced by a placeholder by using Milos Rancic solution:
+                // https://stackoverflow.com/a/55696135
+                // The placeholder comes from Wikimedia Commons:
+                // https://commons.wikimedia.org/wiki/File:No-Image-Placeholder.svg
+                const coverImg = $('<img>').attr('src', result.volumeInfo.imageLinks === undefined ?
+                    'assets/img/no-image-placeholder.png' : `${result.volumeInfo.imageLinks.smallThumbnail}`);
                 const authorAndTitleCell = $('<td>').text(`${result.volumeInfo.authors.join(' & ')} - ${result.volumeInfo.title}`);
-                const descriptionCell = $('<td>').text(result.searchInfo.textSnippet);
-                const addButtonCell = $('<td>');
+
+                // If no short description available, it will be replaced by the longer description. If the longer description also unavailable, a custom text will be displayed.
+                const descriptionCell = $('<td>').text(result.searchInfo === undefined ?
+                    `${(result.volumeInfo.description === undefined ? 'No description available' : result.volumeInfo.description)}` : `${result.searchInfo.textSnippet}`)
+                    .addClass('description-cell');
                 const addButton = $('<button>').text('Add to my collection').addClass('add-collection');
 
+                // Adding an event listener to each "Add to my collection" button. Once one of those button is clicked, the associated data will be stored in the local storage.
                 addButton.on('click', function () {
-                    collection.push(`${result.volumeInfo.authors.join(' & ')} - ${result.volumeInfo.title}`);
+                    const authorAndTitle = `${result.volumeInfo.authors.join(' & ')} - ${result.volumeInfo.title}`;
+                    const coverUrl = result.volumeInfo.imageLinks.smallThumbnail
+                    const bookObject = { 'authorAndTitle': authorAndTitle, 'coverUrl': coverUrl };
+                    collection.push(bookObject);
                     localStorage.setItem('collection', JSON.stringify(collection));
                 })
 
@@ -59,43 +110,39 @@ function searchBook(url) {
         })
 }
 
-//This function controls the menu sections in the navbar. It decides what to show and what to hide once a section is clicked.
-function showNavbarContent() {
-    dictionaryNav.on('click', function () {
-        dictionaryDiv.removeClass('hidden');
-        searchDiv.addClass('hidden');
-        resultsDiv.addClass('hidden');
-        myBooksDiv.addClass('hidden');
-    })
-
-    searchNav.on('click', function () {
-        searchDiv.removeClass('hidden');
-        dictionaryDiv.addClass('hidden');
-        resultsDiv.addClass('hidden');
-        myBooksDiv.addClass('hidden');
-    })
-
-    myBooksNav.on('click', function () {
-        myBooksDiv.removeClass('hidden');
-        dictionaryDiv.addClass('hidden');
-        searchDiv.addClass('hidden');
-        resultsDiv.addClass('hidden');
-    })
-}
-
-showNavbarContent();
-
 // Retrieve data from the local storage and render the collection of books into a table,
 // and adds a checkbox to each book to be able to mark if it's already been read.
 function loadMyBooks() {
+    myBooksTable.text('');
     let storedCollection = JSON.parse(localStorage.getItem('collection'));
+
+    // Filtering the storedCollection array of objects from duplicates by using chickens' solution: 
+    // https://stackoverflow.com/a/56757215
+    let filteredCollection = storedCollection.filter((v, i, a) => a.findIndex(v2 => (JSON.stringify(v2) === JSON.stringify(v))) === i)
+
+    // If nothing is stored in the local storage, the value of storedCollection variable will be equal to an empty array.
+    // If there is/are already books stored in the local storage, those books will be added to the collection array to keep our collection up to date.
+    // Then we can push the new books to the up to date collection array which will be stored in local storage by overwriting the old array there.
+    if (storedCollection === null) {
+        storedCollection = [];
+        filteredCollection = [];
+    } else {
+        filteredCollection.forEach(book => {
+            collection.push(book);
+        })
+    }
+
+    // Rendering our collection of books to the screen
     const myBookList = document.getElementById('my-books-list');
-    for (let i = 0; i < storedCollection.length; i++) {
+    for (let i = 0; i < filteredCollection.length; i++) {
         const rowEl = document.createElement('tr');
         const indexCell = document.createElement('td');
         indexCell.textContent = i + 1;
+        const coverCell = document.createElement('td');
+        const coverImg = document.createElement('img');
+        coverImg.setAttribute('src', filteredCollection[i].coverUrl)
         const authorAndTitleCell = document.createElement('td');
-        authorAndTitleCell.textContent = storedCollection[i];
+        authorAndTitleCell.textContent = filteredCollection[i].authorAndTitle;
         const checkboxCell = document.createElement('td');
         checkboxCell.textContent = 'Read';
         const checkboxEl = document.createElement('input');
@@ -103,39 +150,42 @@ function loadMyBooks() {
         checkboxEl.setAttribute('id', `ch${i + 1}`);
         checkboxEl.classList.add('read');
 
+        coverCell.append(coverImg);
         checkboxCell.append(checkboxEl);
-        rowEl.append(indexCell, authorAndTitleCell, checkboxCell);
+        rowEl.append(indexCell, coverCell, authorAndTitleCell, checkboxCell);
         myBookList.append(rowEl);
     }
-    const saveBtn = document.createElement('button');
-    saveBtn.textContent = 'Save';
-    saveBtn.setAttribute('id', 'saveBtn');
-    myBooksDiv.append(saveBtn);
+
+    // The save button will only be visible if we have at least one book added to the collection
+    if (collection.length > 0) {
+        document.getElementById('saveBtn').classList.remove("hidden");
+    }
 }
 
-// Function to allow storing state whether a book is already read or not.
+// Function to allow storing state whether a book is already read or not by using Betty Miller's solution:
+// https://copyprogramming.com/howto/javascript-save-multiple-checkboxes-with-localstorage
 function saveReadState() {
     saveBtn.addEventListener('click', function () {
         const checkboxes = document.querySelectorAll('input[type="checkbox"]');
         const read = [];
 
+        //if (checkboxes.length > 0) {
         for (let i = 0; i < checkboxes.length; i++) {
             read.push({ id: checkboxes[i].id, checked: checkboxes[i].checked });
         }
+        //}
 
         localStorage.setItem('read', JSON.stringify(read));
     })
 }
 
-// Function to retrieve the state whether a book is already read or not.
+// Function to retrieve the state whether a book is already read or not by using Betty Miller's solution:
+// https://copyprogramming.com/howto/javascript-save-multiple-checkboxes-with-localstorage
 function loadReadState() {
     const readStates = JSON.parse(localStorage.getItem('read'));
-
-    for (let i = 0; i < readStates.length; i++) {
-        document.getElementById(readStates[i].id).checked = readStates[i].checked;
-    };
+    if (readStates !== null) {
+        for (let i = 0; i < readStates.length; i++) {
+            document.getElementById(readStates[i].id).checked = readStates[i].checked;
+        };
+    }
 }
-
-loadMyBooks();
-saveReadState();
-loadReadState();
