31d518ace95f659880a4ae996af3490b5f6db5c2
Ioana Iosub <stu.waltham@gmail.com>
2024-01-26 10:47:25 +0000

    dictionary example fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,16 +22,25 @@ function displayDictionary(event) {
         const example = data[0].meanings[0].definitions[0].example;
 
         searchResult.innerHTML = `<div class="word">
-        <h2 class="sample">${inputValue} <span class="word-details">${data[0].meanings[0].partOfSpeech
-          } ${data[0].phonetic}</span></h2>
+        <h2 class="sample">${inputValue} <span class="word-details">${data[0].meanings[0].partOfSpeech} ${data[0].phonetic}</span></h2>
         <div class="audio">
         <audio controls class="sound" onClick="soundOn"></audio>
         </div>
         </div>
         
         <p class="meaning">${data[0].meanings[0].definitions[0].definition}</p>
-        <p class="example">${example ? example : "No example avaible!"}</p>
+        <p class="example"></p>
         </div>`;
+
+        for (i = 0; i < data[0].meanings[0].definitions.length; i++) {
+          const exampleElement = data[0].meanings[0].definitions[i];
+
+          if (exampleElement.example) {
+            const exampleTxt = document.querySelector(".example");
+            exampleTxt.innerHTML = exampleElement.example;
+            break;
+          }
+        }
 
         for (i = 0; i < data[0].phonetics.length; i++) {
           const element = data[0].phonetics[i];
@@ -55,14 +64,14 @@ const sound = document
 
 // Event listener on the search button to trigger a modal if there's no input value
 // Claudio Redi's solution (edited by unknown user): https://stackoverflow.com/a/11404777
-$('#dictionary-search-btn').on('click', function (e) {
+$("#dictionary-search-btn").on("click", function (e) {
   e.preventDefault();
 
-  $('#noInputModalLabel').modal({ show: false });
-  if ($('#dictionary-search').val() === '') {
-    $('#noInputModalLabel').modal('show');
+  $("#noInputModalLabel").modal({ show: false });
+  if ($("#dictionary-search").val() === "") {
+    $("#noInputModalLabel").modal("show");
     return;
-  };
-})
+  }
+});
 
 // Here will the dictionary work
